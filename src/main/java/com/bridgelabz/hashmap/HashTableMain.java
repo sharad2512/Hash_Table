1ab6949593ba9f6f682464a927108ddff75aa0db
sharad2512 <shnarute95@gmail.com>
2023-02-04 10:14:55 +0530

    ADDED : code to find frequency of word

--- src/main/java/com/bridgelabz/hashmap/HashTableMain.java
+++ src/main/java/com/bridgelabz/hashmap/HashTableMain.java
@@ -1,6 +1,21 @@
 package com.bridgelabz.hashmap;
+import java.util.*;
 public class HashTableMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Hash Table Program");
+        MyHashTable<String, Integer> hashTable = new MyHashTable();
+        String sentence = "To be or not to be";
+        String[] sentenceArray = sentence.toLowerCase().split(" ");
+        for (String word : sentenceArray) {
+            Integer value = hashTable.get(word);
+
+            if( value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hashTable.add(word , value);
+        }
+        System.out.println(hashTable);
     }
+
 }
